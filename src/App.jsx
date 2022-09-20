acc5296055c36197c66aea6577f691205586dcad
codewithjohnson <75730906+codewithjohnson@users.noreply.github.com>
2022-09-20 16:13:14 +0100

    ok

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import "./App.css";
+import React from "react";
+import { useState } from "react";
+
+const intialData = [
+  "love",
+  "joy",
+  "peace",
+  "hope",
+  "fun",
+  "excellence",
+  // "patience",
+  // "goodness",
+  // "faith",
+  // "good",
+];
+
+const App = () => {
+  const [newTag, setNewTag] = useState("");
+  const [tags, setTags] = useState(intialData);
+
+  return (
+    <div>
+      <div className="container">
+        <div className="header">
+          <h2>Tags</h2>
+          <span class="material-symbols-outlined">help</span>
+        </div>
+
+        <div className="input_container">
+          <div className="input">
+            <span class="material-symbols-outlined">loyalty</span>
+            <input
+              type="text"
+              placeholder="enter a new Tag"
+              value={newTag}
+              onChange={(e) => setNewTag(e.target.value)}
+            />
+          </div>
+        </div>
+
+        <div className="result_container">
+          <div className="tagList">
+            {tags.map((tag, index) => (
+              <li key={index} className="tag">
+                <p>{tag}</p>
+                <span>&#128473;</span>
+              </li>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
